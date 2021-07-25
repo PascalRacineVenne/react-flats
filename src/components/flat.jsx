755262f6d3cfab3687cc5461f3ae8e532be142f2
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-07-25 11:50:47 -0400

    stil working on the flat component, trying to retrieve the background image, problem with implementataion of Linear-gradient with jsx...

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Flat = () => {
+const Flat = ({data}) => {
   return (
-    <div className="card" style={"background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';);}">
-      <div className="card-category">150 EUR</div>
+    <div className="card" style={{ backgroundImage: `url('${data.imageUrl}')` }}>
+      <div className="card-category">
+        `{data.price} {data.priceCurrency}
+      </div>
       <div className="card-description">
-        <h2>Super 60m2 in trendy neighborhood!</h2>
+        <h2>{data.name}</h2>
       </div>
       <a className="card-link" href="#"></a>
     </div>
