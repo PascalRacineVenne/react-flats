5541b229e4664dfb075818cb32b770218a20c5c7
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-07-25 12:05:05 -0400

    bakcground image rendering but not displaying since it has no width, need to fix this with ... display flex and splitting the app in two main divs... Flat list and Map

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Flat = ({data}) => {
+// import "./assets/stylesheets/_cards.scss";
+
+const Flat = ({ data }) => {
   return (
     <div className="card" style={{ backgroundImage: `url('${data.imageUrl}')` }}>
-      <div className="card-category">
-        `{data.price} {data.priceCurrency}
-      </div>
+      <div className="card-category">{data.price}</div>
       <div className="card-description">
         <h2>{data.name}</h2>
       </div>
